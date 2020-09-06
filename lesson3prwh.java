c50248893d80a3c9663f3fcf1a2859d5b7f9fa67
bolditsin <bolditsin@gmail.com>
2020-09-06 22:34:10 +0300

    lesson3 homework for pull request

---
+++ lesson3prwh.java
@@ -0,0 +1,10 @@
+package lesson3;
+import java.util.Scanner;
+
+public class lesson3 {
+
+    public static void main(String[] args)
+    {
+        Scanner scanner= new Scanner(System.in);
+    }
+}
